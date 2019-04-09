7469855906d3857a5ae08dfcf58a563af8b61c78
Tolo Torres <tolotorres@gmail.com>
2019-04-09 13:10:03 +0200

    Añadir campos causa y decision

--- helpdesk/models/helpdesk_ticket.py
+++ helpdesk/models/helpdesk_ticket.py
@@ -46,6 +46,12 @@ class HelpdeskTicket(models.Model):
         comodel_name='helpdesk.stage',
         group_expand='_read_group_stage_ids',
         string='Stage')
+    cause = fields.Selection([
+        ('0', 'Causa 0'),
+        ('1', 'Causa 1'),
+        ('2', 'Causa 2'),
+        ('3', 'Causa 3')])
+    decision = fields.Text('Decision Adopted')
 
     def assign_to_me(self):
         self.write({'user_id': self.env.uid})
